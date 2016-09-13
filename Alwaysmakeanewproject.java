8e90675177d8a467926f4eeefb5ac94cae9d3763
Wolfeyes00 <amy.whiteley@sky.com>
2016-09-13 10:46:17 +0100

    Add files via upload

---
+++ Alwaysmakeanewproject.java
@@ -0,0 +1,53 @@
+
+package alwaysmakeanewproject;
+
+
+public class Alwaysmakeanewproject {
+
+    
+    public static void main(String[] args) {
+        int number = 12;
+        int age = 16;
+        int ShoeSize = 5;
+        System.out.println("Your chosen number was:" + number);
+        System.out.println("Your age is:" + age);
+        System.out.println("Your shoesize is:" + ShoeSize);
+        
+        double test= 1.5;
+        double pi = 3.14;
+        System.out.println(test);
+        System.out.println(pi);
+        
+        //area of a circle = pi*r*2
+        //speed = distance/time
+        
+        //*for multiplying
+        // / for division
+        System.out.println(age+ShoeSize);
+        System.out.println(age*ShoeSize);
+        System.out.println(age-ShoeSize);
+        System.out.println(age/ShoeSize);
+        
+        int x =10;
+        int y = 5;
+        
+        if (x < y) {
+            //if the condition true
+            System.out.println("x is less than y");                      
+        }else{
+           //if the condition false
+            System.out.println("x is more than y");
+    
+        
+    }    
+    
+        
+    
+        
+           
+        
+    
+
+        
+    
+}
